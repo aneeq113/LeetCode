9ababa80051d22ce81939b01a87a1d78173f57bc
Muhammad Aneeq <129355791+aneeq113@users.noreply.github.com>
2023-04-01 15:53:41 +0500

    Create Cutting-Ribbons.cpp

---
+++ Binary_Search/Cutting-Ribbons.cpp
@@ -0,0 +1,25 @@
+int maxLength(vector<int>&ribbons,int k)
+{
+  int st=0,en=ribbons.size()+1;
+  while (st+1<en)
+  {
+    int mid=(st+en)/2;
+    if (check(mid,ribbons,k))
+    {
+      st=mid;
+    }
+    else
+    {
+      en=mid;
+  }
+    return (st==0 ?-1:st);
+} 
+bool check(int o,vector<int>&r,int k)
+{
+  int ans=0;
+  for (int i=0;i<r.size();i++) 
+  {
+    ans+=r[i]/o;
+  }
+  return (ans==k);
+}
