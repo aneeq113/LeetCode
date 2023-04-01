467b1bc1955541166d9cd1c4e8008a03757e0f4f
Muhammad Aneeq <129355791+aneeq113@users.noreply.github.com>
2023-04-01 16:14:04 +0500

    Create capacity-to-ship-packages-within-d-days.cpp

---
+++ Binary_Search/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,27 @@
+int shipWithinDays(vector<int>& weights, int days) {
+  int st=0,en=weights.size()+1;
+  while (st+1<en)
+  {
+    int mid=(st+en)/2;
+    if (check(weights,mid,days))
+      st=mid;
+    else
+      en=mid;
+  }
+  return st;
+}
+bool check(vector<int>&w,int o,int k)
+{
+  int ans=0;
+  int z=0;
+  for (int i=0;i<n;i++)
+  {
+    z+=w[i];
+    if (w[i]>=0)
+    {
+      ans++;
+      z=0;
+    }
+  }
+  return (ans==k);
+} 
