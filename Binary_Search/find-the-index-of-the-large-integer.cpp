6d13d884190e780088021cd7009e5452c9a08dc1
Muhammad Aneeq <129355791+aneeq113@users.noreply.github.com>
2023-04-03 14:36:28 +0500

    Update find-the-index-of-the-large-integer.cpp

--- Binary_Search/find-the-index-of-the-large-integer.cpp
+++ Binary_Search/find-the-index-of-the-large-integer.cpp
@@ -1,10 +1,9 @@
-//NOT CORRET YET
 int getIndex(ArrayReader &reader) {
-  int st=0,en=reader.length()+1;
+  int st=0,en=reader.length();
   while (st+1<en)
   {
     int mid=(st+en)/2;
-    int z=reader.comparesub(st,mid,mid+1,en);
+    int z=reader.compareSub(st,mid-1,mid,mid+(mid-st)-1);
     if (z==1)
       en=mid;
     else
