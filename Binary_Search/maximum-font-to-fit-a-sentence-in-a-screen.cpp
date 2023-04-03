af3dd55f9bc646a1a3fb8e0dad9655299374bfab
Muhammad Aneeq <129355791+aneeq113@users.noreply.github.com>
2023-04-03 14:58:29 +0500

    Update maximum-font-to-fit-a-sentence-in-a-screen.cpp

--- Binary_Search/maximum-font-to-fit-a-sentence-in-a-screen.cpp
+++ Binary_Search/maximum-font-to-fit-a-sentence-in-a-screen.cpp
@@ -1,18 +1,28 @@
 int maxFont(string text, int w, int h, vector<int>& fonts, FontInfo fontInfo) {
-        int st=0,en=fonts.size()+1;
+        int st=0,en=fonts.size();
         while (st+1<en)
         {
            int mid=(st+en)/2;
-          int w1=0,int h1=0;
+          long long w1=0,h1=0;
           for (char i:text)
           {
-            h1+=getHeight(i,fonts[mid]);
-            w1+=getWidth(i,fonts[mid]);
+            h1=fontInfo.getHeight(fonts[mid]);
+            w1+=fontInfo.getWidth(fonts[mid],i);
           } 
-          if (h1>=h&&w1>=w)
+          if (h1<=h&&w1<=w)
             st=mid;
           else
             en=mid;
         }
-  return st;
+    int h1=0,w1=0;
+    for (auto i:text)
+    {
+      h1=fontInfo.getHeight(fonts[st]);
+      w1+=fontInfo.getWidth(fonts[st],i);
+    }
+    if (h1<=h&&w1<=w)
+      return fonts[st];
+    else if (st!=0)
+      return fonts[st-1];
+    return -1;
 }
