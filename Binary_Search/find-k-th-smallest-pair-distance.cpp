9583517c43f44624d5597e83bb82add3864b3bbe
Muhammad Aneeq <129355791+aneeq113@users.noreply.github.com>
2023-04-04 07:45:14 +0500

    Update find-k-th-smallest-pair-distance.cpp

--- Binary_Search/find-k-th-smallest-pair-distance.cpp
+++ Binary_Search/find-k-th-smallest-pair-distance.cpp
@@ -1,10 +1,7 @@
 int smallestDistancePair(vector<int>& a, int k) {
         sort(begin(a),end(a));
-        int st=a[1]-a[0];
-        int n=a.size();
-        for (int i=2;i<n;i++)
-            st=min(st,a[i]-a[i-1]);
-        int en=a[n-1]-a[0]+1;
+        int st=0;
+        int en=1e9+10;
         while (st+1<en)
         {
             int mid=(st+en)/2;
